501f0ffd680ff635415a7e838e054a8d987687d1
Nick Landry <nlandry@activenick.net>
2016-11-22 11:04:31 -0500

    Added baseline for PCL with Data models and ViewModels

---
+++ Client/PITCSurveyApp/PITCSurveyApp/PITCSurveyApp.Lib/Model/Survey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PITCSurveyApp.Lib
+{
+    public class Survey
+    {
+        // TO DO: Populate the data class based on the survey schema
+    }
+}
