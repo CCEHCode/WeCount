7617199130fa302489cf85d9364a896679a23425
Nick Landry <nick@mobility42.com>
2017-01-02 16:56:31 -0500

    Adding missing files in PCL project

---
+++ Client/PITCSurveyApp/PITCSurveyApp/PITCSurveyApp.Lib/ViewModels/VolunteerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PITCSurveyApp.Lib.ViewModels
+{
+    // This ViewModel will probably be used for a single Volunteer instance
+    // since there is no need to pull a list of all volunteers from Azure
+
+    class VolunteerViewModel
+    {
+        // Currently not used as the VM are in the shared project
+    }
+}
