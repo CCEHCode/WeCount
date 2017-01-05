963937790fbf4ae885f30de82877a77e3eda6610
Blue Ninja <Blue Ninja@BLUENINJA>
2017-01-05 14:42:35 -0500

    Additional comments for Swagger.

--- Client/PITCSurveyLib/PITCSurveyLib/Constants.cs
+++ Client/PITCSurveyLib/PITCSurveyLib/Constants.cs
@@ -7,9 +7,24 @@ namespace PITCSurveyLib
 	/// </summary>
 	public enum AnswerFormat : int
 	{
+		/// <summary>
+		/// This answer choice needs to additional answer data.
+		/// </summary>
 		None = 0,
+
+		/// <summary>
+		/// This answer choice expects additional string data.
+		/// </summary>
 		String = 1,
+
+		/// <summary>
+		/// This answer choice expects additional integer data.
+		/// </summary>
 		Int = 2,
+
+		/// <summary>
+		/// This answer choice expects an additional date value.
+		/// </summary>
 		Date = 3
 	}
 
