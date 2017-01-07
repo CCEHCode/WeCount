dde587f701964cc727369a439890019f6adeb764
Blue Ninja <Blue Ninja@BLUENINJA>
2017-01-06 22:01:46 -0500

    Created API helper interface for client consumption and simplicity (hopefully).

---
+++ Client/PITCSurveyLib/PITCSurveyLib/APIException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PITCSurveyLib
+{
+	public class APIException : Exception
+	{
+
+		public HttpStatusCode StatusCode { get; private set; }
+
+		public APIException(string Message, HttpStatusCode StatusCode): base(Message)
+		{
+			this.StatusCode = StatusCode;
+		}
+
+		public APIException(string Message, HttpStatusCode StatusCode, Exception InnerException) : base(Message, InnerException)
+		{
+			this.StatusCode = StatusCode;
+		}
+
+		public APIException(string Message) : base(Message)
+		{
+		}
+
+		public APIException(string Message, Exception InnerException) : base(Message, InnerException)
+		{
+		}
+	}
+}
---
+++ Client/PITCSurveyLib/PITCSurveyLib/APIHelper.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Rest;
+using PITCSurveyLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PITCSurveyLib
+{
+	public class APIHelper
+	{
+
+		private const string AzureMobileAppUrl = "https://pitcsurveyapi.azurewebsites.net";
+
+		private PITCSurveyAPI API;
+
+		public APIHelper()
+		{
+			var Creds = new Microsoft.Rest.BasicAuthenticationCredentials() { UserName = "", Password = "" };
+
+			API = new PITCSurveyAPI(new Uri(AzureMobileAppUrl), Creds);
+		}
+
+		public IEnumerable<SurveySummaryModel> GetAvailableSurveys()
+		{
+			return GetAvailableSurveysAsync().Result;
+		}
+
+		public async Task<IEnumerable<SurveySummaryModel>> GetAvailableSurveysAsync()
+		{
+			try
+			{
+				var result = await API.GetAllSurveysAsync(true);
+
+				return result;
+			}
+			catch (HttpOperationException hex)
+			{
+				throw new APIException(hex.Message, hex.Response.StatusCode);
+			}
+			catch (Exception ex)
+			{
+				throw new APIException(ex.Message, ex);
+			}
+		}
+
+		public SurveyModel GetSurveyByID(int ID)
+		{
+			return GetSurveyByIDAsync(ID).Result;
+		}
+
+		public async Task<SurveyModel> GetSurveyByIDAsync(int ID)
+		{
+			try
+			{
+				var result = await API.GetSurveyByIDAsync(ID);
+
+				return result;
+			}
+			catch (HttpOperationException hex)
+			{
+				throw new APIException(hex.Message, hex.Response.StatusCode);
+			}
+			catch (Exception ex)
+			{
+				throw new APIException(ex.Message, ex);
+			}
+		}
+
+		public bool SubmitSurveyResponse(SurveyResponseModel SurveyResponse)
+		{
+			return SubmitSurveyResponseAsync(SurveyResponse).Result;
+		}
+
+		public async Task<bool> SubmitSurveyResponseAsync(SurveyResponseModel SurveyResponse)
+		{
+			try
+			{
+				await API.PostSurveyResponseAsync(SurveyResponse);
+
+				return true;
+			}
+			catch (HttpOperationException hex)
+			{
+				switch (hex.Response.StatusCode)
+				{
+					case System.Net.HttpStatusCode.NoContent:
+						return true;
+					case System.Net.HttpStatusCode.Conflict:
+						return false;
+					default:
+						throw new APIException($"{hex.Message} ({hex.Response.ReasonPhrase}): {hex.Response.Content}", hex.Response.StatusCode, hex);
+				}
+			}
+			catch (Exception ex)
+			{
+				throw new APIException(ex.Message, ex);
+			}
+		}
+
+	}
+}
