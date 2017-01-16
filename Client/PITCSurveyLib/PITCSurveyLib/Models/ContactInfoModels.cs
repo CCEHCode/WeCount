0d993e1f07ca206e99ef493ddbc34c8dc81b6ccf
Blue Ninja <Blue Ninja@BLUENINJA>
2017-01-16 11:27:09 -0500

    Initial contact info models.

---
+++ Client/PITCSurveyLib/PITCSurveyLib/Models/ContactInfoModels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PITCSurveyLib.Models
+{
+	/// <summary>
+	/// Client-side model to retrieve contact info.
+	/// </summary>
+	public class ContactInfoModel
+	{
+		/// <summary>
+		/// General notes to be displayed along with contact info.
+		/// </summary>
+		public string Notes { get; set; }
+
+		/// <summary>
+		/// A list of contacts.
+		/// </summary>
+		public IList<ContactModel> Contacts { get; set; } = new List<ContactModel>();
+	}
+
+	/// <summary>
+	/// Client-side model for a contact's info.
+	/// </summary>
+	public class ContactModel
+	{
+		/// <summary>
+		/// Contact name.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Contact's phone number.
+		/// </summary>
+		public string Phone { get; set; }
+	}
+}
